027888ada78528a32550e8f44d4c5c56b6bf9268
Sergey Ilinykh <62497831+un4g1vable@users.noreply.github.com>
2022-10-20 01:55:22 +0500

    Add files via upload

---
+++ Permutation.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+int main() {
+    setlocale(0, "");
+    string digit;
+    cout << "Введите число: ";
+    cin >> digit;
+    sort(digit.begin(), digit.end());
+    for (int i = digit.length() - 1; i != -1; --i) {
+        cout << digit[i];
+    }
+    cout << " <--- полученное максимальное цисло путём перестановок(сортировки)";
+}
